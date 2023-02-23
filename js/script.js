d0271c13ebfb05134811ab598a24d0225e7df2d4
Lucas Carlos <lucascarlos@lucass-mbp.myfiosgateway.com>
2023-02-22 19:57:15 -0500

    added winner checker

--- js/script.js
+++ js/script.js
@@ -23,6 +23,10 @@ squares.forEach((square, idx) => {
                 game[row][column]++  
                 event.appendChild(span)
                 playerTurn = !playerTurn
+                if(checkWinner(1)){
+                    console.log('you won')
+                    return
+                }
                 setTimeout(() => {
                     computer()
                 }, 0500);
@@ -45,6 +49,11 @@ const computer = () => {
             span.classList.add('choice')
             span.innerHTML = computerChoice
             squares[((row * 3) + column)].appendChild(span)
+            if(checkWinner(-1)){
+                console.log('Computer won')
+                return
+                break
+            }
             playerTurn = !playerTurn
             console.log(game)
             break
@@ -57,6 +66,26 @@ const checkSquares = () => {
     for(let square of squares){
         if(!square.childNodes.length) return true
     }
+    return false
+}
+
+const checkWinner = (piece) => {
+    console.log(game)
+    // horizontal victory
+    if(game[0][0] === piece && game[0][1] === piece && game[0][2] === piece) return true
+    if(game[1][0] === piece && game[1][1] === piece && game[1][2] === piece) return true
+    if(game[2][0] === piece && game[2][1] === piece && game[2][2] === piece) return true
+
+    //vertical victory
+    if(game[0][0] === piece && game[1][0] === piece && game[2][0] === piece) return true
+    if(game[0][1] === piece && game[1][1] === piece && game[2][1] === piece) return true
+    if(game[0][2] === piece && game[1][2] === piece && game[2][2] === piece) return true
+
+    //diagoanal victory
+    if(game[0][0] === piece && game[1][1] === piece && game[2][2] === piece) return true
+    if(game[0][2] === piece && game[1][1] === piece && game[2][0] === piece) return true
+
+    //no winner yet
     return false
 }
 
