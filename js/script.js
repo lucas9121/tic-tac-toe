651d88eadbd0799b29cad2200a13aa7aceb5d7d2
Lucas Carlos <lucascarlos@lucass-mbp.myfiosgateway.com>
2023-02-22 19:57:44 -0500

    trying minMax option

--- js/script.js
+++ js/script.js
@@ -91,4 +91,112 @@ const checkWinner = (piece) => {
 
 
 
+/*
+
+function print_board(board) {
+    console.log("\n");
+    for (let row of board) {
+        console.log(row);
+    }
+}
+
+function evaluate(board) {
+    // check rows for winner
+    for (let row of board) {
+        if (new Set(row).size == 1) {
+            if (row[0] == 'X') {
+                return 10;
+            } else if (row[0] == 'O') {
+                return -10;
+            }
+        }
+    }
+
+    // check columns for winner
+    for (let col = 0; col < 3; col++) {
+        let column = [board[row][col] for (let row = 0; row < 3; row++)];
+        if (new Set(column).size == 1) {
+            if (board[0][col] == 'X') {
+                return 10;
+            } else if (board[0][col] == 'O') {
+                return -10;
+            }
+        }
+    }
+
+    // check diagonals for winner
+    if (new Set([board[i][i] for (let i = 0; i < 3; i++)]).size == 1) {
+        if (board[0][0] == 'X') {
+            return 10;
+        } else if (board[0][0] == 'O') {
+            return -10;
+        }
+    }
+    if (new Set([board[i][2-i] for (let i = 0; i < 3; i++)]).size == 1) {
+        if (board[0][2] == 'X') {
+            return 10;
+        } else if (board[0][2] == 'O') {
+            return -10;
+        }
+    }
+
+    // if no winner
+    return 0;
+}
+
+function minimax(board, depth, alpha, beta, is_maximizing) {
+    let score = evaluate(board);
+
+    if (score == 10) {
+        return score - depth;
+    }
+    if (score == -10) {
+        return score + depth;
+    }
+    if (!board.some(sublist => sublist.includes(null))) {
+        return 0;
+    }
+
+    if (is_maximizing) {
+        let best_score = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == null) {
+                    board[i][j] = 'X';
+                    let score = minimax(board, depth+1, alpha, beta, false);
+                    board[i][j] = null;
+                    best_score = Math.max(best_score, score);
+                    alpha = Math.max(alpha, score);
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+        }
+        return best_score;
+
+    } else {
+        let best_score = Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == null) {
+                    board[i][j] = 'O';
+                    let score = minimax(board, depth+1, alpha, beta, true);
+                    board[i][j] = null;
+                    best_score = Math.min(best_score, score);
+                    beta = Math.min(beta, score);
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+        }
+        return best_score;
+    }
+}
+
+
+*/
+
+
 console.log(game)
